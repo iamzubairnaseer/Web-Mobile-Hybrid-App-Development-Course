bfa4b9982011366a9c32b8f46147b53beeb849da
iamzubairnaseer <zubairnaseer88@gmail.com>
2020-10-09 12:01:54 +0500

    Delete All added

--- public/Assignment9/app.js
+++ public/Assignment9/app.js
@@ -1,5 +1,13 @@
 var list_container = document.getElementById("list")
 
+function addDelete(){
+    if(list_container.childNodes.length>1){
+        document.getElementById("deleteAll").style.display = "flex"
+    }else{
+        document.getElementById("deleteAll").style.display = "none"
+    }
+}
+
 function add_item(){
     var new_value = document.getElementById("new-item").value
     if(new_value != ""){
@@ -35,37 +43,24 @@ function add_item(){
 
         list.appendChild(image)
 
-        // var new_element = document.createElement("p")
-        // var new_item = document.createTextNode(new_value)
-        // new_element.appendChild(new_item)
-
-        // var newBtn = document.createElement("img")
-        // // var btnName = document.createTextNode("Delete")
-        // // newBtn.appendChild(btnName)
-        // newBtn.setAttribute("onclick","delete_item(this)")
-        // newBtn.setAttribute("src","delete.png")
-        // image.appendChild(newBtn)
-
-        // var editBtn = document.createElement("img")
-        // // var editName = document.createTextNode("Edit")
-        // // editBtn.appendChild(editName)
-        // editBtn.setAttribute("onclick","edit_item(this)")
-        // editBtn.setAttribute("src","edit.png")
-        // image.appendChild(editBtn) 
-
-
-        // list.appendChild(new_element)
-
         document.getElementById("new-item").value = ""
+
+        addDelete()
     }
 }
 
 function delete_item(e){
     e.parentNode.parentNode.remove()
     // e.parentNode.remove();
+    addDelete()
 }
 
 function edit_item(e){
     var edit_value = prompt("Enter new value",e.parentNode.previousSibling.firstChild.firstChild.nodeValue)
     e.parentNode.previousSibling.firstChild.firstChild.nodeValue = edit_value
+}
+
+function deleteAll(){
+    list_container.innerHTML = ""
+    document.getElementById("deleteAll").style.display = "none"        
 }
