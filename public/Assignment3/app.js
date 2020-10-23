af316b61cdabefdbe518cd840a60d57d050ebe92
iamzubairnaseer <zubairnaseer88@gmail.com>
2020-10-24 00:57:02 +0500

    updated

---
+++ public/Assignment3/app.js
@@ -0,0 +1,23 @@
+// function chalbhai(){
+    // console.log('hello')
+    // document.getElementById('tries').scrollIntoView()
+// let okay = document.getElementById('okay')
+// okay.addEventListener('focus',()=>{
+
+// console.log(document.getElementById('okay').scrollIntoView().innerHTML)
+
+// setInterval(()=>{
+//     console.log(window.location.hash)
+//     if(window.location.hash == '#okay'){
+//     let dastarkhwan = document.getElementById('dastarkhwan')
+//     let dastarkhwanCount = 0;
+//     let dastarkhwanInterval = setInterval(()=>{
+//         dastarkhwanCount+=11
+//         dastarkhwan.innerHTML = dastarkhwanCount
+//         if(dastarkhwanCount > 150000){
+//             dastarkhwan.innerHTML = '150,000'
+//             clearInterval(dastarkhwanInterval)
+//         }
+//     },1)
+// }
+// },1)
